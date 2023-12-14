4d27420c05be9d5faf573989fd6a082209bba08c
Swati <swati2003mry@gmail.com>
2023-12-14 09:57:30 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import About from "./components/About";
+import Skills from "./components/skills";
+import Education from "./components/Education";
+import Contact from "./components/Contact";
+import Project from "./components/project";
+function App() {
+  return (
+    <div>
+      <About />
+      <Skills />
+      <Project />
+      <Education />
+      <Contact />
+    </div>
+  );
+}
+export default App;
