32a057c4c3ce6f0ff1214ac7f1cfd1921bf8e8a3
Swati <115008404+swatimry@users.noreply.github.com>
2024-07-26 22:15:04 +0530

    Update projects.js

--- src/projects.js
+++ src/projects.js
@@ -5,7 +5,7 @@ const projects = [
     img: "./images/chatapp.png",
     github: "https://github.com/swatimry/chat-frontend",
     desc:"This is a Chatroom,built using React and socket.io, Chatroom allows users to create different chatrooms and join with custom name, event notifications of users joining and leaving chatrooms are popped up periodically",
-    live:"https://darling-valkyrie-1752de.netlify.app/"
+    live:"https://react-chatapp-sockets.netlify.app/"
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ const projects = [
     img: "./images/brewery.png",
     github: "https://github.com/swatimry/brewery-backend",
     desc:"Brewery ,is a E-commerce website providing fully functional site for a Coffee shop with frontend of site made using React and resposive UI and with purely custom backend in Javascript , notable functionalites includes Cart ,Google Authentication ,Payment Integration with Razorpay and detailed Admin Dashboard.You can also use site with test user credentials provided, Test User Credentials  GoogleID- testuser123@brew@gmail.com & password-123@user ",
-    live:"https://famous-yeot-8cf14f.netlify.app/"
+    live:"https://brewery-ecommmerce.netlify.app/"
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ const projects = [
     img: "./images/portf.png",
     github: "https://github.com/swatimry/react-portfolio",
     desc:"A personal Portfolio website , to showcase work experiences and projects done , reflecting all about educational and technical background built using React.",
-    live:"vefsvsv"
+    live:"https://swati-portfolioweb.netlify.app"
   },
   {
     id: 4,
